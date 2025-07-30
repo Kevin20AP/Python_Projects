533be0b936fee860ad5405ee612629105d64f38e
Kevin20AP <apkevin2027@gmail.com>
2025-07-30 13:57:09 -0500

    Initial commit: upload snake game code

---
+++ main.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import time
+
+from food import Food
+from snake import Snake
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+screen.listen()
+screen.onkey(snake.up ,"Up")
+screen.onkey(snake.down ,"Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right ,"Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move_snake()
+    # Move segments from tail to head
+
+
+    if snake.head.distance(food) < 15 :
+        food.refresh()
+        scoreboard.increase_score()
+
+
+
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+ALIGN = "center"
+FONT = ("Courier", 24, "normal")
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, -280)
+        self.update_scoreboard()
+        self.hideturtle()
+
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align= ALIGN, font= FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
